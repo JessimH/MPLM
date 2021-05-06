534d00ba7d0a0967b5affc28a159d3b9fb74b416
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-05-06 14:03:24 +0200

    rollback

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,8 +4,6 @@
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <link rel="icon" href="favicon.png" />
 
     <!-- Bootstrap CSS -->
